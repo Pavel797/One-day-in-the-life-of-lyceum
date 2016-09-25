b970478ecf50d016870e86d846b4641b7a82caf2
pavel <p.vollkkoff@yandex.ru>
2016-09-25 20:25:10 +0400

    create a project

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+        if (Input.GetKey(KeyCode.A) && this.transform.position.z < 3.2f)
+            this.transform.position += new Vector3(0, 0, 0.2f);
+        if (Input.GetKey(KeyCode.D) && this.transform.position.z > -3.2f)
+            this.transform.position += new Vector3(0, 0, -0.2f);
+    }
+}
